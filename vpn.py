dced1de82398db47e6c6cd6e77878ec0c4bcbb60
SEMICOLON <mrkhaled02@gmail.com>
2015-03-04 05:26:49 -0500

    Update vpn.py
    Adding Signal handler

--- vpn.py
+++ vpn.py
@@ -1,11 +1,17 @@
-import mechanize , pexpect  , sys 
+#!/usr/bin/python
+import mechanize , pexpect  , sys ,signal
+def signal_handler(signal, frame):
+    print "\n[-] Exiting"
+    exit()
+signal.signal(signal.SIGINT, signal_handler)
 from bs4 import BeautifulSoup
 usage = ''' 
-usage : python vpn.py 'cert file' 
+[+] usage : python vpn.py 'cert file' 
 '''
 if len(sys.argv) < 2 : 
 	print usage
-	exit(1) 
+	exit(1)
+print "[*] Starting ... " 
 def password() : 
 	br = mechanize.Browser()
 	br.open("http://www.vpnbook.com/freevpn")
@@ -15,18 +21,22 @@ def password() :
 	passw = link[-1].text
 	password = str(passw.split(":")[1][1:])
 	return  password
-vpn = pexpect.spawn("openvpn "+str(sys.argv[1]))
-vpn.timeout=300000000000
-vpn.expect("Username:")
-vpn.sendline("vpnbook")
-vpn.expect("Password:")
-a = password()
-vpn.sendline(a)
-print "[*] Starting ... " 
-while 1 :
-	if "Completed" in vpn.next() : break  
-	else : print  vpn.next()  
+try :
+	vpn = pexpect.spawn("openvpn vpnbook-euro1-tcp80.ovpn")
+	vpn.timeout=300000000000
+	vpn.expect("Username:")
+	vpn.sendline("vpnbook")
+	vpn.expect("Password:")
+	a = password()
+	vpn.sendline(a)
+	print "[+] Password : "+a
+	while 1 :
+		if "Completed" in vpn.next() : break  
+		else : print  vpn.next()  
 
 
-while 1 : 
-	pass
+	while 1 : 
+		pass
+except : 
+	print "[-] Something went wrong :/ "
+	sys.exit(1)
